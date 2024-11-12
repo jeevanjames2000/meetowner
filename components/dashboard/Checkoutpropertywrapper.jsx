82bc8ca5c36696c38f0b83f28d65ee8891b3133b
Mahesh0237 <114992137+Mahesh0237@users.noreply.github.com>
2024-11-12 19:06:43 +0530

    updates

--- components/dashboard/Checkoutpropertywrapper.jsx
+++ components/dashboard/Checkoutpropertywrapper.jsx
@@ -1,31 +1,21 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import Loginwrapper from '../login/Loginwrapper';
 
 function Checkoutpropertywrapper() {
     return (
-        <div className='bg-white rounded-sm mt-4 p-4'>
-            <div className='flex flex-row justify-between items-center'>
-                <p className='text-[#1D3A76] text-sm font-semibold'>Checkout your property</p>
-                <button
-                    className='text-[#287DB0] border-[2px] border-[#287DB0] px-4 py-1 rounded-md text-[12px] font-semibold'
-                    aria-label="View all properties"
-                >
-                    View All
-                </button>
+        <>
+            <div className='bg-white rounded-sm mt-4 p-4'>
+                <div className='flex flex-row justify-between items-center'>
+                    <p className='text-[#1D3A76] text-sm font-semibold'>Checkout your property</p>
+                    <button
+                        className='text-[#287DB0] border-[2px] border-[#287DB0] px-4 py-1 rounded-md text-[12px] font-semibold'
+                        aria-label="View all properties"
+                    >
+                        View All
+                    </button>
+                </div>
             </div>
-            <Swiper
-                spaceBetween={50}
-                slidesPerView={3}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-            </Swiper>
-        </div>
+        </>
     );
 }
 
