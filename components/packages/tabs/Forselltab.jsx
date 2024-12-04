4e598b97fdcee3b1eb4d36fbafc7b0afe774a3dc
gopalakrishna5026 <gopalakrishna0902@gmail.com>
2024-12-04 19:04:31 +0530

    updates

--- components/packages/tabs/Forselltab.jsx
+++ components/packages/tabs/Forselltab.jsx
@@ -85,14 +85,14 @@ function Forsaletab() {
                         </button>
 
                     </div>
-                    <div className=' relative basis-[25%]  border  border-[#909090] rounded-[10px] px-4 py-3'>
+                    <div className=' bg-[#1D3A76] relative basis-[25%]  border  border-[#909090] rounded-[10px] px-4 py-3'>
                         <button className=" absolute -top-11 left-4  flex mx-auto mt-4 bg-[#699BA0]  text-[#ffffff]   h-7 py-1 px-12 text-[14px] font-[400] rounded-tl-md  rounded-tr-md focus:outline-none ">
                             Popular
                         </button>
-                        <p className=' text-[14px] text-center font-[600] text-[#1D3A76] underline underline-offset-4 '>
+                        <p className=' text-[14px] text-center font-[600] text-[#FEFDF8] underline underline-offset-4 '>
                             Prime
                         </p>
-                        <div className='text-[#6D6C6C] text-center  text-[14px] font-[400] space-y-1 pt-2'>
+                        <div className='text-[#FEFDF8] text-center  text-[14px] font-[400] space-y-1 pt-2'>
                             <p >120 days</p>
                             <p >250</p>
                             <p >10x More</p>
