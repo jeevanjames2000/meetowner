f517d36e8a7063bcaf136fc915f2afbac59bc824
gopalakrishna5026 <gopalakrishna0902@gmail.com>
2024-12-04 19:00:08 +0530

    updates

--- components/packages/Packageswrapper.jsx
+++ components/packages/Packageswrapper.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import Packagestabswrapper from './Packagestabswrapper'
-import Enrollpackages from './parts/Enrollpackages'
+import dynamic from 'next/dynamic'
+const Packagestabswrapper = dynamic(() => import('./Packagestabswrapper'))
+const Enrollpackages = dynamic(() => import('./parts/Enrollpackages'))
+const Recommendedplan = dynamic(() => import('./parts/Recommendedplan'))
+const Benefits = dynamic(() => import('./parts/Benefits'))
+const Operates = dynamic(() => import('./parts/Operates'))
+
+
 
 function Packageswrapper() {
     return (
-        <div className=" px-[80px] mt-5 w-full space-y-6 mb-24 ">
+        <div className=" px-[80px] mt-5 w-full space-y-12 mb-24 ">
             <div className='bg-[#E2EAED] mx-auto'>
                 <p className=' text-[20px] text-[#1D3A76] text-center font-bold py-3 w-full'>
                     SELECT THE RIGHT PACKAGE
@@ -12,6 +18,9 @@ function Packageswrapper() {
             </div>
             <Packagestabswrapper />
             <Enrollpackages />
+            <Recommendedplan />
+            <Benefits />
+            <Operates />
         </div>
     )
 }
