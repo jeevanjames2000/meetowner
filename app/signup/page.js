555686c73e64bf73a0f575b39ab32f00c0887d8d
gopalakrishna5026 <gopalakrishna0902@gmail.com>
2024-12-13 19:08:13 +0530

    updates

--- app/signup/page.js
+++ app/signup/page.js
@@ -104,7 +104,6 @@ async function getUsertypesfetch() {
     const response = await Userapi.get('/usertypes');
     const data = response.data;
     console.log('data:', data);
- 
     if (data.status === 'error') {
       let data = {
         status: 'error',
@@ -113,7 +112,6 @@ async function getUsertypesfetch() {
       }
       return data;
     }
- 
     let finaldata = {
       status: 'success',
       message: 'user types fetched successfully',
