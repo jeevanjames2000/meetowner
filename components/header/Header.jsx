1854031f85087c73a39f187f287e88c41040615d
gopalakrishna5026 <gopalakrishna0902@gmail.com>
2024-12-19 15:39:42 +0530

    updates

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -4,10 +4,11 @@ import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 import logo from '@/public/assets/logo.png'
 import Mainnavigation from './Mainnavigation';
-import { IconDeviceIpad, IconHomePlus, IconUser } from '@tabler/icons-react';
-import { motion } from 'framer-motion'
 import { usePathname } from 'next/navigation';
 import { useUserDetails } from '../zustand/useUserDetails';
+import addproperty from  '@/public/assets/addproperty.svg'
+import downloadapp_svg from '@/public/assets/downloadapp_svg.svg'
+import login from '@/public/assets/login.svg'
 function Header() {
     const userInfo = useUserDetails((state) => state.userInfo);
     const isLogged = useUserDetails((state) => state.isLogged);
@@ -54,19 +55,19 @@ function Header() {
                         :
                         <div className="flex flex-row justify-start items-center gap-4">
                             <div className="hidden md:flex flex-row items-center justify-start gap-2">
-                                <IconDeviceIpad color="#1D3A76" size={18} />
+                              <Image src={downloadapp_svg} alt='downloadapp_svg' className='h-[16px] w-fit'/>
                                 <Link href="/downloadapp" className="text-[#1D3A76] text-sm font-medium font-sans">Download App</Link>
                             </div>
                             <div className="hidden md:flex">
-                                <Link href="/addproperty" className="flex flex-row items-center gap-2 p-2 rounded-md">
-                                    <IconHomePlus color="#1D3A76" size={18} />
+                                <Link href="/addproperty" className="flex flex-row items-center justify-start gap-2 p-2 rounded-md">
+                                   <Image src={addproperty} alt='addproperty' className='h-[22px] w-fit'/>
                                     <button className="text-[#1D3A76] text-sm font-medium font-sans">Add Property</button>
                                 </Link>
                             </div>
                             <div>
-                                <Link href="/" className="bg-[#1D3A76] flex flex-row items-center gap-2 py-1 md:py-2 px-2 md:px-4 rounded-md">
-                                    <IconUser color="#fff" size={18} />
-                                    <button className="text-white text-[12px]md:text-sm font-medium font-sans">Login</button>
+                                <Link href="/" className="bg-[#1D3A76] flex flex-row items-center py-1 md:py-2 px-2 md:px-4 rounded-md">
+                                <Image src={login} alt='login' className='h-[16px] w-fit]'/>
+                                <button className="text-white text-[12px]md:text-sm font-medium font-sans">Login</button>
                                 </Link>
                             </div>
                         </div>
