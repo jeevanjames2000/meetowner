53d5b7affcd9d711fe179d6b7e6308339955c0e2
Mahesh0237 <114992137+Mahesh0237@users.noreply.github.com>
2025-01-02 10:28:02 +0530

    Update Header.jsx

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -40,11 +40,11 @@ function Header() {
 
     return (
         <div
-            className={`h-full md:h-[65px] bg-[#F2F2F2] overflow-visible w-full py-2 transition-all duration-1000  ${scrollY > 120 ? 'sticky top-0 z-50' : ''}`}
+            className={`h-full md:h-[65px] bg-[#F2F2F2] overflow-visible w-full py-2 transition-all duration-1000 shadow-xl ${scrollY > 120 ? 'sticky top-0 z-50' : ''}`}
         >
-            <div className="flex flex-row justify-between items-center shadow-xl py-2 bg-[#F2F2F2] px-4 md:px-[4vw] lg:px-[6vw]">
+            <div className="flex flex-row justify-between h-full items-center bg-[#F2F2F2] px-4 md:px-[4vw] lg:px-[6vw]">
                 <Link href="/dashboard">
-                    <Image src={logo} alt={'logo'} height={80} width={120} />
+                    <Image src={logo} alt={'logo'} height={80} width={120} className='3xl:h-20 3xl:w-56 object-cover' />
                 </Link>
                 {
                     isLogged ?
@@ -60,7 +60,7 @@ function Header() {
                                     pathname === '/addproperty' ? null : (
                                         <Link href="/addproperty" className='bg-[#1D3A76] flex flex-row items-center gap-2 p-2 rounded-md'>
                                             {/* <p className='hidden xs:flex'> */}
-                                                <IconHomePlus color='#fff' size={18} />
+                                            <IconHomePlus color='#fff' size={18} />
                                             {/* </p> */}
                                             <button className='text-white text-[12px] md:text-sm 2xl:text-[18px] 3xl:text-[20px] 4xl:text-[22px] font-[700] hidden xs:flex'>Add Property</button>
                                         </Link>
