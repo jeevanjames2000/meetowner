dd4ad83480c0064801857a8b6b4e29362cddb002
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-03-12 04:01:58 -0700

    as

--- app/page.js
+++ app/page.js
@@ -1,22 +1,25 @@
-import Header from '@/components/header/Header'
-import React from 'react'
-import signup_bg from '@/public/assets/approperties-bg.png'
-import Loginform from '@/components/login/Loginform';
-import Authpagecontent from '@/components/shared/Authpagecontent';
+import Header from "@/components/header/Header";
+import React from "react";
+import signup_bg from "@/public/assets/approperties-bg.png";
+import Loginform from "@/components/login/Loginform";
+import Authpagecontent from "@/components/shared/Authpagecontent";
 
 function page() {
   return (
-    <div className='loginpage h-[100vh]' style={{
-      backgroundImage: `url(${signup_bg.src})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-    }}>
+    <div
+      className="loginpage h-[100vh]"
+      style={{
+        backgroundImage: `url(${signup_bg.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <Header />
       <div className="overflow-hidden flex items-center justify-center h-[calc(100vh-65px)] 3xl:h-[calc(100vh-120px)]">
-        <div className=" grid grid-cols-12 w-full gap-[5%] px-4 md:px-[4vw] lg:px-[6vw] items-center justify-between">
+        <div className=" grid grid-cols-12 w-full gap-[10%] px-4 md:px-[4vw] lg:px-[6vw] items-center justify-between">
           {/* Left Column */}
-         <Authpagecontent />
+          {/* <Authpagecontent /> */}
           {/* Right Column */}
           <div className="flex items-center justify-center col-span-12 md:col-span-6 px-3">
             <Loginform />
@@ -24,7 +27,7 @@ function page() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
